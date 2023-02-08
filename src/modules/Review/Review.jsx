bc09e7b804f09527898e09bf7869544c0c17b241
Oleh Honchar <ooollleeeg@gmail.com>
2023-02-08 17:21:36 +0200

    refactoring by hooks react

--- src/modules/Review/Review.jsx
+++ src/modules/Review/Review.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
 import ReviewVariants from './ReviewVariants/ReviewVariants';
 import ReviewResults from './ReviewResults/ReviewResults';
@@ -7,60 +7,55 @@ import NotificationMessage from './NotificationMessage/NotificationMessage';
 
 import styles from './review.module.scss';
 
-class Review extends Component {
-  state = {
+const Review = () => {
+  const [reviews, setReviews] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
-  };
+  });
 
-  leaveFeedback = status => {
-    this.setState(prevState => {
-      return { [status]: prevState[status] + 1 };
+  const { good, neutral, bad } = reviews;
+
+  const total = reviews.good + reviews.neutral + reviews.bad;
+
+  const leaveFeedback = status => {
+    setReviews(prevState => {
+      const value = prevState[status];
+      return { ...prevState, [status]: value + 1 };
     });
   };
 
-  countTotalFeedback() {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
-    return total;
-  }
-
-  countPositiveFeedbackPercentage(good) {
-    const total = this.countTotalFeedback();
-    const value = this.state.good;
+  const calcPercentage = propName => {
+    const value = reviews[propName];
     const result = ((value / total) * 100).toFixed(2);
     return Number(result);
-  }
+  };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const goodPercent = this.countPositiveFeedbackPercentage('good');
-    return (
-      <div>
-        <h1 className={styles.title}>Cafe "Espresso" feedback</h1>
-        <div className={styles.wrapper}>
-          <ReviewBlock title="Please leave feedback">
-            <ReviewVariants leaveFeedback={this.leaveFeedback} />
-          </ReviewBlock>
-          <ReviewBlock title="Statistics">
-            {total === 0 ? (
-              <NotificationMessage message="There is no feedback" />
-            ) : (
-              <ReviewResults
-                good={good}
-                neutral={neutral}
-                bad={bad}
-                total={total}
-                goodPercent={goodPercent}
-              />
-            )}
-          </ReviewBlock>
-        </div>
+  const goodPercent = calcPercentage('good');
+
+  return (
+    <div>
+      <h1 className={styles.title}>Cafe "Espresso" feedback</h1>
+      <div className={styles.wrapper}>
+        <ReviewBlock title="Please leave feedback">
+          <ReviewVariants leaveFeedback={leaveFeedback} />
+        </ReviewBlock>
+        <ReviewBlock title="Statistics">
+          {total === 0 ? (
+            <NotificationMessage message="There is no feedback" />
+          ) : (
+            <ReviewResults
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              goodPercent={goodPercent}
+            />
+          )}
+        </ReviewBlock>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Review;
